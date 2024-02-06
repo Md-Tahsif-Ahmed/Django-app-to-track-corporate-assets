11ad51a87c2472c18b7a55ef5f452ac0fb487e4f
tahsif <tahsif.cse@gmail.com>
2024-02-07 00:21:00 +0600

    initial setup and create model and fields

---
+++ Track_Asset/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TrackAssetConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Track_Asset'
---
+++ Track_Asset/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# create Company Model
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+# create Employee Model
+class Employee(models.Model):
+    name=models.CharField(max_length=100)
+    company=models.ForeignKey(Company, on_delete=models.CASCADE)
+    designation=models.CharField(max_length=100)
+
+# create Device Model
+class Device(models.Model):
+    type=models.CharField(max_length=50)
+    model=models.CharField(max_length=100)
+    serial_no=models.CharField(max_length=100)
+    company=models.ForeignKey(Company, on_delete=models.CASCADE)
+
+# create Assignment Model
+class Assignment(models.Model):
+    device=models.ForeignKey(Device, on_delete=models.CASCADE)
+    employee=models.ForeignKey(Employee, on_delete=models.CASCADE)
+    checkout_date=models.DateTimeField()
+    return_date=models.DateTimeField(null=True, blank=True)
+    condition_on_checkout=models.TextField()
+    condition_on_return=models.TextField(null=True, blank=True)
+
+
+
+
