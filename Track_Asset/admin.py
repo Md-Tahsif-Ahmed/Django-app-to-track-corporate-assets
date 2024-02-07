d23bcb6cd9bd3de1243826a5a00e94ae6b154b6a
tahsif <tahsif.cse@gmail.com>
2024-02-07 07:44:44 +0600

    admin register model and serializer

--- Track_Asset/admin.py
+++ Track_Asset/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Company)
+admin.site.register(models.Employee)
+admin.site.register(models.Device)
+admin.site.register(models.Assignment)
